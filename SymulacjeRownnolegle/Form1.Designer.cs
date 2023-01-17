e158ed98acead1e608a7dfc66019e6d7a7abd1b3
Eryk <eryk658@wp.pl>
2023-01-17 21:45:19 +0100

    add threading and code improvments

--- SymulacjeRownnolegle/Form1.Designer.cs
+++ SymulacjeRownnolegle/Form1.Designer.cs
@@ -31,6 +31,17 @@
             this.components = new System.ComponentModel.Container();
             this.panel1 = new System.Windows.Forms.Panel();
             this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.button1 = new System.Windows.Forms.Button();
+            this.timer2 = new System.Windows.Forms.Timer(this.components);
+            this.button2 = new System.Windows.Forms.Button();
+            this.timer3 = new System.Windows.Forms.Timer(this.components);
+            this.button3 = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
             this.SuspendLayout();
             // 
             // panel1
@@ -44,15 +55,114 @@
             // 
             // timer1
             // 
-            this.timer1.Enabled = true;
             this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(1064, 50);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "button1";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // timer2
+            // 
+            this.timer2.Tick += new System.EventHandler(this.timer2_Tick);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(1055, 114);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(83, 30);
+            this.button2.TabIndex = 2;
+            this.button2.Text = "button2";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // timer3
+            // 
+            this.timer3.Interval = 1000;
+            this.timer3.Tick += new System.EventHandler(this.timer3_Tick);
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(1054, 171);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(83, 32);
+            this.button3.TabIndex = 3;
+            this.button3.Text = "button3";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(1052, 249);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(106, 16);
+            this.label1.TabIndex = 6;
+            this.label1.Text = "Population count";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(1061, 277);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(0, 16);
+            this.label2.TabIndex = 7;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(1164, 249);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(101, 16);
+            this.label3.TabIndex = 8;
+            this.label3.Text = "Left group count";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(1164, 277);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(0, 16);
+            this.label4.TabIndex = 9;
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(1280, 249);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(111, 16);
+            this.label5.TabIndex = 10;
+            this.label5.Text = "Right group count";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(1281, 277);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(0, 16);
+            this.label6.TabIndex = 11;
             // 
             // Form1
             // 
-            this.ClientSize = new System.Drawing.Size(1151, 520);
+            this.ClientSize = new System.Drawing.Size(1394, 520);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
             this.Controls.Add(this.panel1);
             this.Name = "Form1";
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
@@ -60,6 +170,17 @@
 
         private System.Windows.Forms.Panel panel1;
         private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Timer timer2;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Timer timer3;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label6;
     }
 }
 
--- SymulacjeRownnolegle/Form1.cs
+++ SymulacjeRownnolegle/Form1.cs
@@ -5,6 +5,7 @@ using System.Data;
 using System.Drawing;
 using System.Linq;
 using System.Text;
+using System.Threading;
 using System.Threading.Tasks;
 using System.Windows.Forms;
 
@@ -13,8 +14,16 @@ namespace SymulacjeRownnolegle
 
     public partial class Form1 : Form
     {
-        public List<Person> people;
+        public Population population;
         public int person_spped = 5;
+        public int person_radius = 5;
+        public int start_population = 6;
+
+
+        Random rnd = new Random();
+        Thread_A thread_object_a;
+        ThreadStart thread_start_a;
+        Thread thread_a;
         public Form1()
         {
             InitializeComponent();
@@ -24,70 +33,183 @@ namespace SymulacjeRownnolegle
         private void panel1_Paint(object sender, PaintEventArgs e)
         {
             InitializePeople();
-            RedrawPeople();
+            RecalculatePeoplePositions();
+            DrawPopulation();
+            
         }
 
-        private void RedrawPeople()
+        private void RecalculatePeoplePositions()
         {
-            Graphics g0 = this.panel1.CreateGraphics();
-            g0.Clear(Color.White);
-
-            Pen blackPen = new Pen(Color.Black, 3);
-            g0.DrawLine(blackPen, 20, 100, 500, 100);
-            g0.DrawLine(blackPen, 20, 300, 500, 300);
-
-            Random rnd = new Random();
-            foreach (Person person in this.people)
+            
+            foreach (Person person in this.population.people)
             {
                 if (person.status == 1)
                 {
-                    person.DrawPerson(this.panel1);
-                    int new_center_x = person.center_x + person.speed;
-                    int rand_value = rnd.Next(-person.speed, person.speed);
+                    
+                    int rand_value = rnd.Next(-person.speed, person.speed + 1);
+                    Console.WriteLine(rand_value);
                     int new_center_y = person.center_y + rand_value;
-                    //Person new_person = new Person(new_center_x, new_center_y, 20);
-                    if((100 < new_center_y - person.radius) && (new_center_y + person.radius < 300) ) // && (person.start_y - 25 < new_center_y) && (person.start_y + 25 > new_center_y)
+                    int new_center_x = person.center_x + person.x_direction;
+                    Person new_person = new Person(new_center_x, new_center_y, person.radius, person.group);
+                    bool wall_collision = new_person.CheckCollisionWithWalls();
+                    if(!wall_collision)
                     {
-                        bool collision = person.CheckCollisionBetweenNew(this.people, new_center_x, new_center_y);
-                           
+                        bool collision = person.CheckCollisionBetweenNew(this.population.people, new_center_x, new_center_y);
+
                         if (!collision)
                         {
                             person.center_x = new_center_x;
                             person.center_y = new_center_y;
-                            Console.WriteLine(person.center_x);
                         }
 
-                        if (person.center_x >= person.end_point_x)
+                        if (person.center_x >= person.end_point_x && person.group == "left")
+                        {
                             person.status = 0;
-                        
+                            this.population.population_count -= 1;
+                            this.population.left_group -= 1;
+                        }
+
+                        if (person.center_x <= person.end_point_x && person.group == "right")
+                        {
+                            person.status = 0;
+                            this.population.population_count -= 1;
+                            this.population.right_group -= 1;
+                            
+                        }
+                            
                     }
+                    //if ((100 < new_center_y - person.radius) && (new_center_y + person.radius < 300) ) // && (person.start_y - 25 < new_center_y) && (person.start_y + 25 > new_center_y)
                 }
             }
-            
-
         }
 
         private void timer1_Tick(object sender, EventArgs e)
         {
-            RedrawPeople();
+            RecalculatePeoplePositions();
+            DrawPopulation();
+            label2.Text = this.population.population_count.ToString();
+            label4.Text = this.population.left_group.ToString();
+            label6.Text = this.population.right_group.ToString();
+            
         }
 
         private void InitializePeople()
         {
             List<Person> people = new List<Person>();
             Random rnd = new Random();
-            while(people.Count<3)
+            while(people.Count<this.start_population)
             {
                 int y = rnd.Next(100, 300);
-                Person new_person = new Person(10, y, 20);
+                Person new_person = new Person(Global.RightEntranceX, y, this.person_radius, "right");
                 bool start_collision = new_person.CheckCollisionBetweenExistingPeople(people);
                 bool wall_collision = new_person.CheckCollisionWithWalls();
                 
                 if(!start_collision && ! wall_collision)
                     people.Add(new_person);
             }
-            this.people = people;
+            Population population = new Population(people);
+            population.population_count = this.start_population;
+            population.left_group = this.start_population;
+            this.population = population;
         }
+
+        public void InitializeNewPerson()
+        {
+            if(this.population.population_count < this.population.max_population_count)
+            {
+                int group_loss = this.rnd.Next(0, 2);
+                int y = this.rnd.Next(100, 300);
+                Person new_person;
+                if(group_loss==0)
+                    new_person = new Person(Global.LeftEntranceX, y, this.person_radius, "left");
+                else
+                    new_person = new Person(Global.RightEntranceX, y, this.person_radius, "right");
+                bool start_collision = new_person.CheckCollisionBetweenExistingPeople(this.population.people);
+                bool wall_collision = new_person.CheckCollisionWithWalls();
+
+                if (!start_collision && !wall_collision)
+                {
+                    this.population.people.Add(new_person);
+                    if (new_person.group == "left")
+                        this.population.left_group += 1;
+                    else
+                        this.population.right_group += 1;
+                    this.population.population_count += 1;
+                }
+                    
+            } 
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            
+            if(thread_a == null)
+            {
+                this.population.run_simulation = true;
+                thread_object_a = new Thread_A(this.population, this.panel1);
+                thread_start_a = new ThreadStart(thread_object_a.RedrawPeople);
+                thread_a = new Thread(thread_start_a);
+                timer2.Enabled = true;
+                button1.Text = "Stop simulation";
+                thread_a.Start();
+
+            }
+            else
+            {
+                this.population.run_simulation = false;
+                thread_a.Join();
+                thread_a.Interrupt();
+            }
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            if(timer1.Enabled)
+            {
+                timer1.Enabled = false;
+                //timer3.Enabled = false;
+            }
+            else
+            {
+                timer1.Enabled = true;
+                //timer3.Enabled = true;
+            }
+                
+        }
+
+        private void timer2_Tick(object sender, EventArgs e)
+        {
+            if(this.population.run_simulation)
+                DrawPopulation();
+        }
+
+        public void DrawPopulation()
+        {
+            Graphics g0 = panel1.CreateGraphics();
+            g0.Clear(Color.White);
+            Pen blackPen = new Pen(Color.Black, 20);
+            g0.DrawLine(blackPen, 20, 100, 500, 100);
+            g0.DrawLine(blackPen, 20, 300, 500, 300);
+
+            foreach (Person person in this.population.people)
+            {
+                if(person.status==1)
+                {
+                    person.DrawPerson(panel1);
+                }
+            }
+        }
+
+        private void timer3_Tick(object sender, EventArgs e)
+        {
+            InitializeNewPerson();
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            Console.WriteLine(7 + -3);
+        }
+
     }
     public class Person
     {
@@ -95,19 +217,32 @@ namespace SymulacjeRownnolegle
         public int radius;
         public int status;
         public int speed;
+        public int x_direction;
+        public string group;
 
-        public Person(int center_x, int center_y, int radius)
+        public Person(int center_x, int center_y, int radius, string group)
         {
             Random rnd = new Random();
             this.radius = radius;
+            this.speed = 2;
             this.center_x = center_x;
             this.center_y = center_y;
             this.start_x = center_x;
             this.start_y = center_y;
-            this.end_point_x = 500;
+            this.group = group;
+            if (this.group == "left")
+            {
+                this.x_direction = speed;
+                this.end_point_x = Global.RightEntranceX;
+            }
+            else
+            {
+                this.x_direction = -speed;
+                this.end_point_x = Global.LeftEntranceX;
+            }
             this.end_point_y = rnd.Next(200, 450);
             this.status = 1;
-            this.speed = 2;
+            
         }
 
         public bool CheckCollisionBetweenExistingPeople(List<Person> people)
@@ -129,7 +264,7 @@ namespace SymulacjeRownnolegle
         public bool CheckCollisionBetweenNew(List<Person> people, int center_x, int center_y)
         {
             bool collision = false;
-            Person new_person = new Person(center_x, center_y, 20);
+            Person new_person = new Person(center_x, center_y, 20, "left");
             foreach (Person p in people)
             {
                 if (this != p && p.status == 1)
@@ -146,7 +281,7 @@ namespace SymulacjeRownnolegle
         public bool CheckCollisionWithWalls()
         {
             bool collision = false;
-            if ((this.center_y - this.radius < 100) && (this.center_y + this.radius > 300))
+            if ((this.center_y - this.radius < 100) || (this.center_y + this.radius > 300))
                 collision = true;
             return collision;
         }
@@ -157,6 +292,91 @@ namespace SymulacjeRownnolegle
             Pen pen = new Pen(Color.Red);
             Graphics g0 = panel.CreateGraphics();
             g0.DrawEllipse(pen, this.center_x - this.radius / 2, this.center_y - this.radius / 2, this.radius * 2, this.radius * 2);
+        }
+    }
+
+    public class Population
+    {
+        public List<Person> people;
+        public int max_population_count = 30;
+        public int population_count = 0;
+        public int right_group = 0;
+        public int left_group = 0;
+        public int person_spped = 5;
+        public int person_radius = 5;
+        public bool run_simulation = false;
+
+        public Population(List<Person> people)
+        {
+            this.people= people;
+        }
+    }
+
+    static class Global
+    {
+        private static int _left_entrance_x = 10;
+        private static int _right_entrance_x = 500;
+
+        public static int LeftEntranceX
+        {
+            get { return _left_entrance_x; }
+            set { _left_entrance_x = value; }
+        }
+        public static int RightEntranceX
+        {
+            get { return _right_entrance_x; }
+            set { _right_entrance_x = value; }
+        }
+    }
+    public class Thread_A
+    {
+        public Population population;
+        public Panel panel;
+
+        public Thread_A(Population population, Panel panel)
+        {
+            this.population = population;
+            this.panel = panel;
+        }
+        public void RedrawPeople()
+        {
+            while (this.population.run_simulation)
+            {
+                //Graphics g0 = this.panel.CreateGraphics();
+                //g0.Clear(Color.White);
+                //Pen blackPen = new Pen(Color.Black, 20);
+                //g0.DrawLine(blackPen, 20, 100, 500, 100);
+                //g0.DrawLine(blackPen, 20, 300, 500, 300);
+
+                //Random rnd = new Random();
+                //foreach (Person person in this.population.people)
+                //{
+                //    if (person.status == 1)
+                //    {
+                //        //person.DrawPerson(this.panel);
+                //        int new_center_x = person.center_x + person.speed;
+                //        int rand_value = rnd.Next(-person.speed, person.speed);
+                //        int new_center_y = person.center_y + rand_value;
+                //        Person new_person = new Person(new_center_x, new_center_y, 5);
+                //        bool wall_collision = new_person.CheckCollisionWithWalls();
+                //        if (!wall_collision)
+                //        {
+                //            bool collision = person.CheckCollisionBetweenNew(this.population.people, new_center_x, new_center_y);
+
+                //            if (!collision)
+                //            {
+                //                person.center_x = new_center_x;
+                //                person.center_y = new_center_y;
+                //            }
+
+                //            if (person.center_x >= person.end_point_x)
+                //                person.status = 0;
+                //        }
+                //        //if ((100 < new_center_y - person.radius) && (new_center_y + person.radius < 300) ) // && (person.start_y - 25 < new_center_y) && (person.start_y + 25 > new_center_y)
+                //    }
+                //}
+                //System.Threading.Thread.Sleep(100);
+            }
         }
     }
 }
